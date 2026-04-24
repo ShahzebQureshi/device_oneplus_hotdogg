ef28bb59487c3127caeb2f6fcbbd09553ec8a87f
ShahzebQureshi <55016172+ShahzebQureshi@users.noreply.github.com>
2026-04-24 10:38:11 +0500

    Update and rename yaap_hotdog.mk to yaap_hotdogg.mk

--- yaap_hotdogg.mk
+++ yaap_hotdogg.mk
@@ -15,20 +15,13 @@ $(call inherit-product, device/oneplus/hotdogg/device.mk)
 $(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-<<<<<<<< HEAD:yaap_hotdog.mk
-PRODUCT_NAME := yaap_hotdog
-PRODUCT_DEVICE := hotdog
-========
-PRODUCT_NAME := lineage_hotdogg
+PRODUCT_NAME := yaap_hotdogg
 PRODUCT_DEVICE := hotdogg
->>>>>>>> 2b81b82 (hotdogg: Initial bringup):lineage_hotdogg.mk
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := HD1925
 PRODUCT_MANUFACTURER := OnePlus
-
 PRODUCT_SYSTEM_NAME := OnePlus7TProNR
 PRODUCT_SYSTEM_DEVICE := OnePlus7TProNR
-
 PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
@@ -39,16 +32,10 @@ TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 
 # Build info
-<<<<<<<< HEAD:yaap_hotdog.mk
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=OnePlus7TPro \
-    TARGET_NAME=OnePlus7TPro
-========
 BUILD_FINGERPRINT := "OnePlus/OnePlus7TProNR/OnePlus7TProNR:11/RKQ1.201022.002/2108021431:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=OnePlus7TProNR \
     TARGET_PRODUCT=OnePlus7TProNR \
     PRIVATE_BUILD_DESC="OnePlus7TProNR-user 11 RKQ1.201022.002 2108021431 release-keys"
->>>>>>>> 2b81b82 (hotdogg: Initial bringup):lineage_hotdogg.mk
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
